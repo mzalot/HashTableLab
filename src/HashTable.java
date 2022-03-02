bb5523d8feb3e7e660beab406032ef096265d13f
mzalot <69329263+mzalot@users.noreply.github.com>
2022-03-02 11:42:28 -0700

    I added instance data and a default constructor.

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,2 +1,20 @@
+/**
+ HashTable
+ The program will create a HashTable
+ Author: Mareks Zeile
+ Collaborator(s):
+ Collaboration:
+ Date: 2/2/22
+ On My Honor, I confirm that I followed all collaboration policy guidelines, and that the work I am submitting is my own: MZ
+ **/
+
+import java.util.ArrayList;
 public class HashTable {
+    //instance data
+    private ArrayList<String> hashTable;
+
+    //default constructor
+    public HashTable(){
+        hashTable = new ArrayList();
+    }
 }
