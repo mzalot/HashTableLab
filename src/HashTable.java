fdf01268a9aebc1bee2e6e644c4588dea35904bf
mzalot <69329263+mzalot@users.noreply.github.com>
2022-03-02 12:33:14 -0700

    working on the methods in the class

--- src/HashTable.java
+++ src/HashTable.java
@@ -7,14 +7,34 @@
  Date: 2/2/22
  On My Honor, I confirm that I followed all collaboration policy guidelines, and that the work I am submitting is my own: MZ
  **/
+import java.lang.Math;
 
-import java.util.ArrayList;
 public class HashTable {
     //instance data
-    private ArrayList<String> hashTable;
+    //array
+    private String[] hashTable;
+    //buckets filled
+    int bucketsFilled;
 
-    //default constructor
-    public HashTable(){
-        hashTable = new ArrayList();
+    //constructor
+    public HashTable(int capacity){
+        hashTable = new String[capacity];
+        bucketsFilled = 0;
+    }
+
+    //put method
+    public boolean put(String key, String value){
+        boolean fail = false;
+        //int value of key
+        Integer keyVal = Integer.valueOf(key);
+        int indexValue = (int) (hashTable.length % (Math.pow(2,keyVal)));
+        //if buckets are filled. Grow the array
+        if(bucketsFilled == hashTable.length){
+            String[] newHashTable = new String[hashTable.length * 2];
+            for(int i=0; i<hashTable.length; i++){
+                newHashTable[i] = hashTable[i];
+            }
+        }
+        return fail;
     }
 }
