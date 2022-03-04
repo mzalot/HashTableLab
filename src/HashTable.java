d04f9a5e73295b535c5b686a86c0dfa854c07a04
mzalot <69329263+mzalot@users.noreply.github.com>
2022-03-04 09:19:50 -0700

    I created all of the methods

--- src/HashTable.java
+++ src/HashTable.java
@@ -24,10 +24,9 @@ public class HashTable {
 
     //put method
     public boolean put(String key, String value){
-        boolean fail = false;
-        //int value of key
-        Integer keyVal = Integer.valueOf(key);
-        int indexValue = (int) (hashTable.length % (Math.pow(2,keyVal)));
+        boolean success = false;
+        //hashcode value
+        int hash = hashCode(key);
         //if buckets are filled. Grow the array
         if(bucketsFilled == hashTable.length){
             String[] newHashTable = new String[hashTable.length * 2];
@@ -35,6 +34,29 @@ public class HashTable {
                 newHashTable[i] = hashTable[i];
             }
         }
-        return fail;
+        if (hashTable[hash] == "") {
+            success = true;
+            hashTable[hash] = value;
+        }
+        return success;
+    }
+    //get hashes the key to get the index, and returns that element. Returns null if key not found.
+    //get method
+    public String get(String key){
+        //hashcode value
+        int hash = hashCode(key);
+        if(hashTable[hash] != ""){
+            return hashTable[hash];
+        }else{
+            return null;
+        }
+    }
+
+    //returns the unique int in the range of the [0, array length)
+    private int hashCode(String key){
+        //int value of key
+        Integer keyVal = Integer.valueOf(key);
+        int indexValue = (int) (hashTable.length % (Math.pow(2,keyVal)));
+        return indexValue;
     }
 }
